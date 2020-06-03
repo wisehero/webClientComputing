6964737fbfc316ea7f1c3bcb2a835415c8b76890
wisehero <godjw96@gmail.com>
2020-06-03 23:49:39 +0900

    Create app.js

---
+++ Assignment11/practice3/app.js
@@ -0,0 +1,7 @@
+const para = document.querySelector("#text");
+
+para.addEventListener("click", function () {
+  para.style.fontSize = "50px";
+  para.style.color = "yellow";
+  para.style.backgroundColor = "blue";
+});
